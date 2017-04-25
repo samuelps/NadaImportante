94b299e16b4c8c41107b0a321a7264b778a37de8
Samuel Perez Saldivar <sperez@woodward.com.mx>
2017-04-24 23:18:54 -0500

    Iniciando pagina mamona de mi

---
+++ chocosam/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chocosam</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+<body>
+
+<?php 
+echo 'Bienvenido a mi self <br>';
+?>
+
+<div id="sideBar">
+	Esta sera la side bar
+
+</div>
+
+<div>
+	Esto sera el turbo container central del centro
+</div>
+
+<div>esto sera el footer too feo</div>
+</body>
+</html>
