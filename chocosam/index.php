42bc138d2574ff1ad19d24c7161bde3ceaff9d80
Samuel Perez Saldivar <sperez@woodward.com.mx>
2017-04-28 15:03:18 -0500

    Estarteandotl

--- chocosam/index.php
+++ chocosam/index.php
@@ -11,16 +11,16 @@
 
 
 <div id="sideBar">
-	<div id="perfil">Perfil</div>
-	<div id="CV">CV</div>
-	<div id="log">Log del averno</div>
+	<div id="perfil">Perfil del ORTO</div>
+	<div id="CV" class="menu">CV</div>
+	<div id="log" class="menu">Logs del averno del averno</div>
 
 </div>
 <div  id="mainContainer">
 <?php 
 echo 'Estoy creando esto <br>';
 ?>
-	Esto no es 
+	Sueno sukistrukis, sone que habia lagos grandes de agua cristalina pero extremadamente salada, una compania lograba crear un prosceso que convertia el agua salada derectamente en energiatl, y se daba un monopolio de agua, y de poderio militar por ese recurso, robots avanzados eran creados, uno de esos robots adquiria conciencia y le gustaba ver pechos de humanas.
 </div>
 
 </body>
