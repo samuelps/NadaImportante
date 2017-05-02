b9b5e0c7625a5ad56001b905da78982bfdaa5274
Samuel Perez Saldivar <samuel.per.sal@gmail.com>
2017-05-02 18:26:15 -0500

    font awesome

--- chocosam/index.php
+++ chocosam/index.php
@@ -2,26 +2,33 @@
 <html>
 <head>
 	<title>Chocosam</title>
+	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/turbocss.css">
 	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
 <body>
 
-
-
-<div id="sideBar">
-	<div id="perfil">Perfil del ORTO</div>
-	<div id="CV" class="menu">CV</div>
-	<div id="log" class="menu">Logs del averno del averno</div>
-
-</div>
-<div  id="mainContainer">
-<?php 
-echo 'Estoy creando esto <br>';
-?>
-	Sueno sukistrukis, sone que habia lagos grandes de agua cristalina pero extremadamente salada, una compania lograba crear un prosceso que convertia el agua salada derectamente en energiatl, y se daba un monopolio de agua, y de poderio militar por ese recurso, robots avanzados eran creados, uno de esos robots adquiria conciencia y le gustaba ver pechos de humanas. XD
-</div>
+	<div id="sideBar">
+		<div id="perfil">Perfil del ORTO</div>
+		<div id="CV"  class="menu">Ridiculum Vitae</div>
+		<div id="log" class="menu">
+			<i class="fa fa-book fa-6" aria-hidden="true"></i>Log del averno
+		</div>
+		<div id="log" class="menu">1001 Mandamientos</div>
+		<div class="menu"
+	
+	</div>
+	<div class="container"  id="mainContainer">
+		<h1> ¿Para que existe esto?</h1>
+		
+		<p>Esto existe por el mero hecho de que quiero que exista, como pedes apreciar, esta pagina carece de funcionalidades utiles para el publico en general,pero eso lo hace divertido, </p>
+	<?php 
+	echo 'Estoy creando esto <br>';
+	?>
+		Sueno sukistrukis, sone que habia lagos grandes de agua cristalina pero extremadamente salada, una compania lograba crear un prosceso que convertia el agua salada derectamente en energiatl, y se daba un monopolio de agua, y de poderio militar por ese recurso, robots avanzados eran creados, uno de esos robots adquiria conciencia y le gustaba ver pechos de humanas. XD
+	</div>
 
 </body>
 </html>
